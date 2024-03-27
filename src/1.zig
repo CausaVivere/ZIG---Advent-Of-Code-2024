16febe716470af52779091af05dd6d65b66f6c33
CausaVivere <paulsirazvan098@yahoo.com>
2024-03-28 00:15:59 +0200

    Day One

---
+++ src/1.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn Solution() anyerror!void {
+    // Read File
+    var file = try std.fs.cwd().openFile("src/inputs/day1_calibration.txt", .{});
+    defer file.close();
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+    var buf: [1024]u8 = undefined;
+
+    var sum: u16 = 0;
+
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        var firstNumber: u8 = 0;
+        var secondNumber: u8 = 0;
+        var count: u16 = 0;
+
+        for (line) |char| {
+            if (std.ascii.isDigit(char)) {
+                if (count == 0) {
+                    firstNumber = char;
+                    if (secondNumber == 0) secondNumber = char;
+                } else {
+                    secondNumber = char;
+                }
+                count += 1;
+            }
+        }
+
+        const combinedNumber: u16 = (firstNumber - '0') * 10 + (secondNumber - '0');
+
+        sum += combinedNumber;
+    }
+    print("Day One Result: {d}", .{sum});
+    print("\n", .{});
+}
---
+++ src/main.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+const dayOne = @import("1.zig");
+
+pub fn main() !void {
+    try dayOne.Solution();
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
